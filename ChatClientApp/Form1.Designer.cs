f74e12a9f4d2e4ed1dfc24f62e28e4e298655226
Mokana <mokana0409@gmail.com>
2026-05-11 21:03:30 +0530

    Initial commit

---
+++ ChatClientApp/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ChatClientApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLang;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtChat;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLang;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLang = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtChat = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLang = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(80, 17);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(150, 22);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtLang
+            // 
+            this.txtLang.Location = new System.Drawing.Point(320, 17);
+            this.txtLang.Name = "txtLang";
+            this.txtLang.Size = new System.Drawing.Size(80, 22);
+            this.txtLang.TabIndex = 3;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(420, 15);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(100, 27);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtChat
+            // 
+            this.txtChat.Location = new System.Drawing.Point(20, 60);
+            this.txtChat.Multiline = true;
+            this.txtChat.Name = "txtChat";
+            this.txtChat.ReadOnly = true;
+            this.txtChat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtChat.Size = new System.Drawing.Size(500, 300);
+            this.txtChat.TabIndex = 5;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(20, 370);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(380, 22);
+            this.txtMessage.TabIndex = 6;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(420, 370);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(100, 25);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Send";
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(20, 20);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(47, 16);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name:";
+            // 
+            // lblLang
+            // 
+            this.lblLang.AutoSize = true;
+            this.lblLang.Location = new System.Drawing.Point(250, 20);
+            this.lblLang.Name = "lblLang";
+            this.lblLang.Size = new System.Drawing.Size(71, 16);
+            this.lblLang.TabIndex = 1;
+            this.lblLang.Text = "Language:";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(550, 420);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblLang);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtLang);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtChat);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Chat Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ ChatClientApp/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatClientApp
+{
+    public partial class Form1 : Form
+    {
+        TcpClient client;
+        NetworkStream stream;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = txtName.Text.Trim();
+                string lang = txtLang.Text.Trim();
+
+                client = new TcpClient();
+                await client.ConnectAsync("127.0.0.1", 5000);
+                stream = client.GetStream();
+
+                byte[] nameData = Encoding.UTF8.GetBytes(name);
+                await stream.WriteAsync(nameData, 0, nameData.Length);
+
+                byte[] langData = Encoding.UTF8.GetBytes(lang);
+                await stream.WriteAsync(langData, 0, langData.Length);
+
+                txtChat.AppendText("Connected to server.\r\n");
+                Task.Run(() => ReceiveMessages());
+            }
+            catch (Exception ex)
+            {
+                txtChat.AppendText("Error: " + ex.Message + "\r\n");
+            }
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string msg = txtMessage.Text.Trim();
+                if (string.IsNullOrEmpty(msg)) return;
+
+                byte[] data = Encoding.UTF8.GetBytes(msg);
+                await stream.WriteAsync(data, 0, data.Length);
+                txtMessage.Clear();
+            }
+            catch
+            {
+                txtChat.AppendText("Disconnected from server.\r\n");
+            }
+        }
+
+        private void ReceiveMessages()
+        {
+            byte[] buffer = new byte[2048];
+            int bytes;
+
+            while (true)
+            {
+                try
+                {
+                    bytes = stream.Read(buffer, 0, buffer.Length);
+                    if (bytes == 0) break;
+
+                    string msg = Encoding.UTF8.GetString(buffer, 0, bytes);
+                    AppendText(msg);
+                }
+                catch
+                {
+                    AppendText("Connection lost.\r\n");
+                    break;
+                }
+            }
+        }
+
+        void AppendText(string text)
+        {
+            if (InvokeRequired)
+                Invoke(new Action(() => txtChat.AppendText(text + "\r\n")));
+            else
+                txtChat.AppendText(text + "\r\n");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
